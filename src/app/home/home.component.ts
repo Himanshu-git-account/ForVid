9ab1295075775314f6c3801386e99b7b45273c3e
Nikita <ntomilingeek@gmail.com>
2018-12-29 19:16:48 +0200

    Added: services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import {MDCTextField} from '@material/textfield';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,22 @@ export class HomeComponent implements OnInit {
 	@ViewChild('emailForm') emailFormEl: ElementRef;
 	public emailForm;
 
-  constructor() {}
+	public isSmallScreen= false;
+
+  constructor(public breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {
   	this.emailForm = new MDCTextField(this.emailFormEl.nativeElement);
+
+  	this.breakpointObserver
+      .observe(['(max-width: 992px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.isSmallScreen = true;
+        } else {
+          this.isSmallScreen = false;
+        }
+      });
   }
 
 }
